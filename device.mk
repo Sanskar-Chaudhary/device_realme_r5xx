236a4f7bbbb0472645eaf66d72e8a85f590ce77d
takumi021 <omrameshpatel@gmail.com>
2022-08-08 17:34:46 +0000

    r5x: Use CAF Bluetooth Stack from LA.UM.9.11.r1-04500-NICOBAR.0 Update Aptx Blobs from redfin-user 11 RQ1A.210205.004 7038034 release-keys Uprev Manifest stuff
    Co-authored-by: takumi021 <omrameshpatel@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -57,15 +57,13 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
-
-PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-
-PRODUCT_PACKAGES += \
-    libbthost_if
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_qti \
+    libbtconfigstore \
+    libbthost_if \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
